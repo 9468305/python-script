4583b970b170cbb4b4fa716f3ea24dbaf7516129
chenqib <chenqib@Ctrip.com>
2018-01-01 00:15:42 +0800

    [cross_fetch] Implementation

--- cross_fetch/cf.py
+++ cross_fetch/cf.py
@@ -14,46 +14,36 @@ import requests
 logging.getLogger(__name__).addHandler(NullHandler())
 logging.basicConfig(level=logging.DEBUG)
 
-CTOOL_API = r'https://so.ctool.cc/api/api.php?ver=3&kw={}&page={}&num={}'
-CTOOL_VER = 3
+CTOOL_API = r'https://so.ctool.cc/api/api.php?ver=3&kw={}&page={}&num={}&site={}'
 CTOOL_PAGE = 1
 CTOOL_NUM = 20
 
 
 def ctool(session, keyword, page, num, site):
     '''
-    GET ctool by params
+    GET ctool url + params
     '''
-    kw = base64.b64encode(keyword.encode()).decode()
-    _url = CTOOL_API.format(kw, page, num)
-#    params = [('ver', CTOOL_VER),
-#              ('kw', base64.b64encode(keyword.encode())),
-#              ('page', page),
-#              ('num', num)]
-#    if site:
-#        params.append(['site', site])
+    kw_string = ' '.join(keyword)
+    kw_param = base64.b64encode(kw_string.encode()).decode()
+    _url = CTOOL_API.format(kw_param, page, num, site)
     _response = session.get(_url)
     logging.debug('Response code = ' + str(_response.status_code))
     if _response.status_code != 200:
         return None
-    _json = base64.b64decode(_response.text).decode()
-    logging.debug(_json)
-    return _json
+    _json_text = base64.b64decode(_response.text).decode()
+    _json_obj = json.loads(_json_text)
+    logging.debug(_json_obj)
+    return _json_obj
 
 
 def cross_fetch(keyword, site):
-    '''Fetch keyword by Search Engine.'''
+    '''Cross fetch keyword by Search Engine.'''
     with requests.Session() as session:
-        kw_string = ' '.join(keyword)
-        _result = ctool(session, kw_string, CTOOL_PAGE, CTOOL_NUM, site)
-        fetch = json.loads(_result, encoding='utf-8')
-        logging.debug(fetch)
-
+        fetch = ctool(session, keyword, CTOOL_PAGE, CTOOL_NUM, site)
         keyword.reverse()
-        kw_string = ' '.join(keyword)
-        _result = ctool(session, kw_string, CTOOL_PAGE, CTOOL_NUM, site)
-        cross = json.loads(_result, encoding='utf-8')
-        logging.debug(cross)
+        cross = ctool(session, keyword, CTOOL_PAGE, CTOOL_NUM, site)
+        #TODO Add cross verify
+        #TODO ctool API fail
 
 
 if __name__ == "__main__":
